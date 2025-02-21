cedcca37bf67dc57e2e671f5b8287f9fc61d1ffb
yixiaoyu <yixiaoyu@stu.pku.edu.cn>
2025-02-21 17:27:00 +0800

    feat: add polyffusion

---
+++ polyffusion/ddpm/utils.py
@@ -0,0 +1,18 @@
+"""
+---
+title: Utility functions for DDPM experiment
+summary: >
+  Utility functions for DDPM experiment
+---
+# Utility functions for [DDPM](index.html) experiemnt
+"""
+
+import torch
+import torch.utils.data
+
+
+def gather(consts: torch.Tensor, t: torch.Tensor):
+    """Gather consts for $t$ and reshape to feature map shape"""
+    consts = consts.to(t.device)
+    c = consts.gather(-1, t)
+    return c.reshape(-1, 1, 1, 1)
